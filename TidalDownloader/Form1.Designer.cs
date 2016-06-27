0452d282e0d5c300e5b238a1691e366c9a7d1c67
Alex Janka <alex@alexjanka.com>
2016-06-27 13:52:40 +1000

    Started build - works for all without a "suffix" in their segment URI (ie. Famous, New Workout Plan but not Lemonade)

--- TidalDownloader/Form1.Designer.cs
+++ TidalDownloader/Form1.Designer.cs
@@ -28,12 +28,206 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.URI = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.downloadButton = new System.Windows.Forms.Button();
+            this.output = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.startNo = new System.Windows.Forms.NumericUpDown();
+            this.endNo = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.stitchButton = new System.Windows.Forms.Button();
+            this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.pickFolder = new System.Windows.Forms.Button();
+            this.outfile = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.suffix = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.startNo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.endNo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // URI
+            // 
+            this.URI.Location = new System.Drawing.Point(12, 29);
+            this.URI.Name = "URI";
+            this.URI.Size = new System.Drawing.Size(260, 20);
+            this.URI.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "TIDAL URI segment";
+            // 
+            // downloadButton
+            // 
+            this.downloadButton.Location = new System.Drawing.Point(12, 152);
+            this.downloadButton.Name = "downloadButton";
+            this.downloadButton.Size = new System.Drawing.Size(97, 23);
+            this.downloadButton.TabIndex = 2;
+            this.downloadButton.Text = "Download";
+            this.downloadButton.UseVisualStyleBackColor = true;
+            this.downloadButton.Click += new System.EventHandler(this.clickDownload);
+            // 
+            // output
+            // 
+            this.output.Location = new System.Drawing.Point(115, 122);
+            this.output.Multiline = true;
+            this.output.Name = "output";
+            this.output.ReadOnly = true;
+            this.output.Size = new System.Drawing.Size(157, 194);
+            this.output.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 195);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Start:";
+            // 
+            // startNo
+            // 
+            this.startNo.Location = new System.Drawing.Point(50, 192);
+            this.startNo.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.startNo.Name = "startNo";
+            this.startNo.Size = new System.Drawing.Size(59, 20);
+            this.startNo.TabIndex = 5;
+            // 
+            // endNo
+            // 
+            this.endNo.Location = new System.Drawing.Point(50, 219);
+            this.endNo.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.endNo.Name = "endNo";
+            this.endNo.Size = new System.Drawing.Size(59, 20);
+            this.endNo.TabIndex = 7;
+            this.endNo.Value = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 221);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "End:";
+            // 
+            // stitchButton
+            // 
+            this.stitchButton.Location = new System.Drawing.Point(12, 293);
+            this.stitchButton.Name = "stitchButton";
+            this.stitchButton.Size = new System.Drawing.Size(97, 23);
+            this.stitchButton.TabIndex = 8;
+            this.stitchButton.Text = "Stitch";
+            this.stitchButton.UseVisualStyleBackColor = true;
+            this.stitchButton.Click += new System.EventHandler(this.stitchButton_Click);
+            // 
+            // pickFolder
+            // 
+            this.pickFolder.Location = new System.Drawing.Point(13, 123);
+            this.pickFolder.Name = "pickFolder";
+            this.pickFolder.Size = new System.Drawing.Size(96, 23);
+            this.pickFolder.TabIndex = 9;
+            this.pickFolder.Text = "Choose Folder";
+            this.pickFolder.UseVisualStyleBackColor = true;
+            this.pickFolder.Click += new System.EventHandler(this.pickFolder_Click);
+            // 
+            // outfile
+            // 
+            this.outfile.Location = new System.Drawing.Point(12, 267);
+            this.outfile.Name = "outfile";
+            this.outfile.Size = new System.Drawing.Size(97, 20);
+            this.outfile.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 251);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Filename:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 59);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(33, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Suffix";
+            // 
+            // suffix
+            // 
+            this.suffix.Location = new System.Drawing.Point(12, 75);
+            this.suffix.Name = "suffix";
+            this.suffix.Size = new System.Drawing.Size(260, 20);
+            this.suffix.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(284, 328);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.suffix);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.outfile);
+            this.Controls.Add(this.pickFolder);
+            this.Controls.Add(this.stitchButton);
+            this.Controls.Add(this.endNo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.startNo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.output);
+            this.Controls.Add(this.downloadButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.URI);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "TIDAL Downloader";
+            ((System.ComponentModel.ISupportInitialize)(this.startNo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.endNo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox URI;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button downloadButton;
+        private System.Windows.Forms.TextBox output;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown startNo;
+        private System.Windows.Forms.NumericUpDown endNo;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button stitchButton;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
+        private System.Windows.Forms.Button pickFolder;
+        private System.Windows.Forms.TextBox outfile;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox suffix;
     }
 }
 
--- TidalDownloader/Form1.cs
+++ TidalDownloader/Form1.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Diagnostics;
 
 namespace TidalDownloader
 {
@@ -15,6 +18,89 @@ namespace TidalDownloader
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void clickDownload(object sender, EventArgs e)
+        {
+            WebClient wc = new WebClient();
+
+            if(folderBrowserDialog.SelectedPath == null | folderBrowserDialog.SelectedPath == "")
+            {
+                output.AppendText("Error: no path selected\n");
+                return;
+            }
+
+            if(URI.Text == null | URI.Text == "")
+            {
+                output.AppendText("Error: no partial Tidal URI\n");
+                return;
+            }
+
+            String loc = folderBrowserDialog.SelectedPath;
+
+            for (int i = (int)startNo.Value; i < (int)endNo.Value + 1; i++)
+            {
+                output.AppendText("Downloading section #" + i.ToString() + "\n");
+
+                try
+                {
+                    wc.DownloadFile(URI.Text + i.ToString() + ".ts" + suffix.Text, loc + "\\" + i.ToString() + ".ts");
+                }
+                catch
+                {
+                    output.AppendText("Error downloading section #" + i.ToString() + "\n");
+                    break;
+                }
+            }
+        }
+
+        private void pickFolder_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog.ShowDialog();
+        }
+
+        private void stitchButton_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog.SelectedPath == null | folderBrowserDialog.SelectedPath == "")
+            {
+                output.AppendText("Error: no path selected\n");
+                return;
+            }
+
+            if (outfile.Text == null | outfile.Text == "")
+            {
+                output.AppendText("Error: no output filename\n");
+                return;
+            }
+
+            String command = " -i \"concat:";
+
+            for (int i = (int)startNo.Value; i < (int)endNo.Value + 1; i++)
+            {
+                command += folderBrowserDialog.SelectedPath + "\\" + i.ToString() + ".ts";
+
+                if (i != (int)endNo.Value)
+                {
+                    command += "|";
+                }
+            }
+
+            command += "\" -c copy -bsf aac_adtstoasc \"" + folderBrowserDialog.SelectedPath + "\\" + outfile.Text + ".mp4\"";
+
+            Console.WriteLine(command);
+
+            var proc1 = new ProcessStartInfo();
+            proc1.UseShellExecute = true;
+
+            proc1.WorkingDirectory = folderBrowserDialog.SelectedPath;
+
+            proc1.FileName = @"C:\Program Files\ImageMagick-6.9.3-Q16\ffmpeg.exe";
+            proc1.Verb = "runas";
+            proc1.Arguments = command;
+            proc1.WindowStyle = ProcessWindowStyle.Hidden;
+            Process.Start(proc1);
+
+            //Process.Start("ffmpeg", command);
         }
     }
 }
